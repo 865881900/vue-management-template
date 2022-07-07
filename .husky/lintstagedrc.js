7eed73bc4cb0b2585628cbfb2379862fcad9bc6c
wang865881900 <865881900@qq.com>
2022-07-07 11:24:55 +0800

    fix: 1: 修改环境变量在浏览器环境不能读取的bug feat: 动态路由配置

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -3,6 +3,6 @@ module.exports = {
     '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
     'package.json': ['prettier --write'],
     '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
-    '*.{scss,less,styl}': ['stylelint --fix', 'prettier --write'],
+    '*.{scss,less,styl}': ['stylelint --fix --custom-syntax postcss-scss', 'prettier --write'],
     '*.md': ['prettier --write']
 };
